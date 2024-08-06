0d63d0bd4e1a539af76047f9b3a14c00e36b6f37
jngmnj <exorbitance@naver.com>
2024-08-06 17:52:43 +0900

    feat: complete main page, category/tag post list page, start implementing infinite scroll

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,10 +1,11 @@
 import { Post } from "@/types";
 import { cn } from "@/utils/style";
+import { format } from "date-fns";
 import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
 
-type PostCardProps = Post & {
+type PostCardProps = Omit<Post, 'tags'> & {
     className?: string;
 }
 
@@ -19,19 +20,29 @@ const PostCard: FC<PostCardProps> = ({
   className,
 }) => {
   return (
-    <Link href={`/posts/${id}`}>
-      <a className={cn('bg-white', className)}>
-        <Image src={preview_image_url} width={0} height={0} sizes="100vw" alt={title} className="h-auto w-full" />
-        <h2 className="text-xl font-bold">{title}</h2>
-        <p className="mt-2 text-gray-600">{content}</p>
+    <Link href={`/posts/${id}`} className={cn('bg-white', className)}>
+      <div className="relative aspect-[1.8/1] w-full">
+        <Image
+          src={preview_image_url ?? '/default.png'}
+          fill
+          sizes="360px"
+          alt={title}
+          priority={true}
+          className="object-cover"
+        />
+      </div>
+      <div className="p-2">
+        <span className="text-xs py-1 px-2 bg-slate-100 rounded-md mb-2">{category}</span>
+        <h2 className="text-lg font-medium mb-3">{title}</h2>
+        <p className="line-clamp-3 text-sm text-gray-500">{content}</p>
         <div className="mt-4 text-sm text-gray-500">
-          <span>{category}</span>
-          <span className="mx-1">•</span>
-          <span>{tags.join(', ')}</span>
+          <span className="mx-1">
+            {format(new Date(created_at), 'yyyy년 M월 d일 HH:mm')}
+          </span>
         </div>
-      </a>
+      </div>
     </Link>
   );
 };
 
-export default PostCard
+export default PostCard;
